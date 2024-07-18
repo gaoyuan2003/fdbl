f147716f95891def1e9b7bdaec7fc26ad271b6cc
枫丹白露 <115710396+gaoyuan2003@users.noreply.github.com>
2024-07-18 11:12:18 +0800

    update

--- js/set.js
+++ js/set.js
@@ -75,8 +75,8 @@ function getBgImg() {
 let bg_img_preinstall = {
     "type": "1", // 1:默认背景 2:每日一图 3:随机风景 4:随机动漫
     "2": "https://api.dujin.org/bing/1920.php", // 每日一图
-    // "3": "https://api.btstu.cn/sjbz/api.php", // 随机风景
-    "3": "https://blingcc1.eu.org/api/img",
+    "3": "https://api.btstu.cn/sjbz/api.php", // 随机风景
+    // "3": "https://blingcc1.eu.org/api/img",
     "4": "https://www.dmoe.cc/random.php" // 随机动漫
 };
 
